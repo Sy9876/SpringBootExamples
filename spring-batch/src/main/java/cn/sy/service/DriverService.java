f5b63b081978ad2818f6865849571830efe27da6
Sy9876 <shenyue_sam@163.com>
2018-09-19 06:11:05 +0800

    add batch

---
+++ spring-batch/src/main/java/cn/sy/service/DriverService.java
@@ -0,0 +1,29 @@
+package cn.sy.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import cn.sy.dto.Driver;
+import cn.sy.service.dao.DriverDao;
+
+@Service
+public class DriverService {
+
+	@Autowired
+	private DriverDao driverDao;
+
+    public Driver getDriver(String phoneNo) {
+    	
+    	Driver d = null;
+    	
+    	System.out.println("DriverService getDriver start. phoneNo=" + phoneNo);
+    	try {
+    		d=driverDao.findByPhoneNo(phoneNo);
+    		//System.out.println(d);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	System.out.println("DriverService getDriver  end. return: " + d);
+    	return d;
+    }
+
+}
